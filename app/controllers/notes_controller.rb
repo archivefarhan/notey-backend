1c167284ce3d1076dd0902d5ef39fc6ca1de1f37
farhan <farhanj.mohamed@gmail.com>
2023-03-19 12:34:20 -0700

    added crud actions to note controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,44 @@
 class NotesController < ApplicationController
+  def index
+    notes = Note.All
+    render json: notes.as_json
+  end
+
+  def create
+    note = Note.create!(name: params[:name], body: params[:body])
+
+    if note.save
+      render json: note.as_json
+    else
+      render json: { errors: note.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    id = params[:id]
+    note = Note.find(id)
+
+    render json: note.as_json
+  end
+
+  def update
+    id = params[:id]
+    note = Note.find(id)
+
+    note.name = params[:name] || note.name
+    note.body = params[:body] || note.body
+
+    note.save
+
+    render json: note.as_json
+  end
+
+  def destroy
+    id = params[:id]
+    note = Note.find(id)
+
+    note.destroy
+
+    render json: { message: "note deleted" }
+  end
 end
